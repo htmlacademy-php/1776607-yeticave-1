0397ecbf892742532dc55a14f9c83a530b378b80
Stepan Kormilin <ubscoder@gmail.com>
2026-05-26 19:51:03 +0300

    functions

--- init.php
+++ init.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 require_once __DIR__ . '/config/constants.php';
-require_once __DIR__ . '/includes/helpers.php';
-require_once __DIR__ . '/includes/view.php';
-require_once __DIR__ . '/includes/database.php';
-require_once __DIR__ . '/includes/bootstrap.php';
+require_once __DIR__ . '/functions/helpers.php';
+require_once __DIR__ . '/functions/view.php';
+require_once __DIR__ . '/functions/database.php';
+require_once __DIR__ . '/functions/bootstrap.php';
